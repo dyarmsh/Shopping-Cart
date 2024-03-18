68a02521eecf47af9e1e497a11d21ab8b6ac5687
dram0027/usr/local/bin/python3 <dram0027@student.monash.edu>
2024-03-18 22:01:46 +1100

    Adding Auth and UI logic

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js"
 
 let appSettings = {
     databaseURL: "https://shopping-cart-app-55ad7-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 const myApp = initializeApp(appSettings)
+const myAuth = getAuth(myApp)
+const provider = new GoogleAuthProvider()
 const myDatabase = getDatabase(myApp)
 const shoppingListInDB = ref(myDatabase, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addToCartBtn = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
-const containerEl = document.getElementById("container2")
+const signInWithGoogleEl = document.getElementById("sign-in-with-google-btn")
+const viewLoggedIn = document.getElementById("view-logged-in")
+const viewLoggedOut = document.getElementById("view-logged-out")
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      viewLoggedIn.style.display = "block"
+      viewLoggedOut.style.display = "none"
+  
+    } else {
+      viewLoggedIn.style.display = "none"
+      viewLoggedOut.style.display = "block"
+    }
+  })
+  
+  signInWithGoogleEl.addEventListener("click", function() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log("Yay it worked")
+      }).catch((error) => {
+        console.log("Nooo it didnt work")
+      })
+  })
 
 // adding to cart lists the item below
 addToCartBtn.addEventListener("click", function() {
